27066b8b6eea6df9ee3f6009611eff9dc65217e0
Steve Tetchoup <stevetcthoup20@gmail.com>
2025-10-22 16:20:45 +0100

    add dependencies in pom XML

---
+++ src/main/java/com/st/backend_OidcUserService/BackendOidcUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.st.backend_OidcUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOidcUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOidcUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/st/backend_OidcUserService/BackendOidcUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.st.backend_OidcUserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOidcUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
